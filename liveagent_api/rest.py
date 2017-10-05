ba08a9286345f14f35ac77862a54a867fe3209e3
Michal Bebjak <mbebjak@qualityunit.com>
2017-10-05 12:46:30 +0200

    hardcoded pool size

--- liveagent_api/rest.py
+++ liveagent_api/rest.py
@@ -99,7 +99,7 @@ class RESTClientObject(object):
 
         # https pool manager
         self.pool_manager = urllib3.PoolManager(
-            num_pools=pools_size,
+            num_pools=9999,
             cert_reqs=cert_reqs,
             ca_certs=ca_certs,
             cert_file=cert_file,
