e30d1ba2985fe766536f9dc4f161a77d35fe718d
ArtemFo <afomenko@qualityunit.com>
2022-09-22 18:04:09 +0300

    feat: Add forward to IvrStep
    #13085

--- liveagent_api/models/ivr_step.py
+++ liveagent_api/models/ivr_step.py
@@ -33,21 +33,24 @@ class IvrStep(object):
     swagger_types = {
         'type': 'str',
         'params': 'str',
-        'choices': 'list[IvrChoice]'
+        'choices': 'list[IvrChoice]',
+        'forward': 'IvrForward'
     }
 
     attribute_map = {
         'type': 'type',
         'params': 'params',
-        'choices': 'choices'
+        'choices': 'choices',
+        'forward': 'forward'
     }
 
-    def __init__(self, type=None, params=None, choices=None):  # noqa: E501
+    def __init__(self, type=None, params=None, choices=None, forward=None):  # noqa: E501
         """IvrStep - a model defined in Swagger"""  # noqa: E501
 
         self._type = None
         self._params = None
         self._choices = None
+        self._forward = None
         self.discriminator = None
 
         self.type = type
@@ -55,6 +58,8 @@ class IvrStep(object):
             self.params = params
         if choices is not None:
             self.choices = choices
+        if forward is not None:
+            self.forward = forward
 
     @property
     def type(self):
@@ -128,6 +133,27 @@ class IvrStep(object):
         """
 
         self._choices = choices
+
+    @property
+    def forward(self):
+        """Gets the forward of this IvrStep.  # noqa: E501
+
+
+        :return: The forward of this IvrStep.  # noqa: E501
+        :rtype: IvrForward
+        """
+        return self._forward
+
+    @forward.setter
+    def forward(self, forward):
+        """Sets the forward of this IvrStep.
+
+
+        :param forward: The forward of this IvrStep.  # noqa: E501
+        :type: IvrForward
+        """
+
+        self._forward = forward
 
     def to_dict(self):
         """Returns the model properties as a dict"""
